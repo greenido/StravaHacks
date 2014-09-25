584aa829bd87fc07d8aa84b4b7ef5273b0366034
Ido Green <greenido@gmail.com>
2014-09-25 15:52:10 +0300

    Add a loop to fetch pages

--- gOAuth-test.js
+++ gOAuth-test.js
@@ -42,6 +42,64 @@ function getStravaService() {
 }
 
 //
+// Fetching all the activities and put them inside our sheet of activities.
+//
+// @see: http://strava.github.io/api/v3/activities/#get-activities
+//
+function getActivities() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var aSheet = ss.getSheetByName("Activities");
+  
+  var sService = getStravaService();
+  
+  var startLine = 2;
+  var resPerPage = 100;
+  
+  for (var i=1; i < 200; i++) {
+    Logger.log("** Fetching page: " + i);
+    var response = UrlFetchApp.fetch('https://www.strava.com/api/v3/athlete/activities?per_page=' + resPerPage + '&page=' + i, {
+       headers: { Authorization: 'Bearer ' + sService.getAccessToken() }
+      });
+    var jsonData = response.getContentText();
+    //Logger.log("Now we can make requests to the Strava API.\n" + JSON.stringify(jsonData));
+
+    var retCode = jsonToSheet(jsonData, startLine, aSheet);
+    if (retCode == -1) {
+      Logger.log("Goting to stop fetching on page: "+i+ " as we don't have any more activities");
+      return;
+    }
+    startLine += resPerPage;
+  }
+}
+
+//
+//
+//
+function jsonToSheet(jsonData, startLine, sheet) {
+
+  var dataSet = JSON.parse(jsonData);
+  var rows = [];
+  var data;
+  if ( dataSet.length == 0) {
+    Logger.log("No more activities on start line: "+startLine);
+    return -1;
+  }
+  
+  for (i = 0; i < dataSet.length; i++) {
+    data = dataSet[i];
+    rows.push([data.id, data.start_date_local , data.location_city, data.location_country , 
+               data.name, data.distance, data.moving_time, data.elapsed_time ,  data.total_elevation_gain ,data.kudos_count, data.average_speed,
+               data.achievement_count, data.type ]);
+  }
+  Logger.log("We got: " + rows.length + " activities");
+  dataRange = sheet.getRange(startLine, 1, rows.length, 13);
+  dataRange.setValues(rows);
+  SpreadsheetApp.flush();
+  return 0;
+}
+
+
+//
 //
 //
 function authCallback(request) {
